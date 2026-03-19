94e4e9d267a046deceb9e57e4bc4385e774c8d60
NikhilYadavCoder <nikhilgolla9@gmail.com>
2026-03-19 23:40:59 +0530

    Initial setup: JWT authentication and project configuration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SkillBridge.Models;
+
+namespace SkillBridge.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure User-UserProfile relationship
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.UserProfile)
+                .WithOne(up => up.User)
+                .HasForeignKey<UserProfile>(up => up.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
